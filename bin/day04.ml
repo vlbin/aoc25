6d65234f9252568e80bff183904c5b47f5405f38
vlbin <albinlandgreen@gmail.com>
2025-12-04 10:00:08 +0100

    day 4 part 2

--- bin/day04.ml
+++ bin/day04.ml
@@ -13,6 +13,15 @@ let get_coord yi xi map =
 let get_line yi map = 
   List.hd (List.drop yi map)
 
+let replace i list =
+  List.mapi (fun j e -> if i = j then "." else e) list
+
+let replace_coord yi xi map =
+  let before = List.take yi map in
+  let changed = replace xi (List.nth map yi) in
+  let after = List.drop (yi + 1) map in
+  List.concat (before :: (changed :: []) :: after :: [])
+
 let dirs = [
   (-1, -1); (-1, 0); (-1, 1);
   (0, -1); (0, 1);
@@ -27,24 +36,42 @@ let get_neighbor_indices yi xi map =
 
 let get_neighboring_rolls yi xi map =
   match get_coord yi xi map with 
-  | "." -> 99
+  | "." -> ((yi,xi), 9)
   | _ -> 
     let indices = get_neighbor_indices yi xi map in
-    List.map (fun (a,b) -> get_coord a b map) indices
+    let len = List.map (fun (a,b) -> get_coord a b map) indices
     |> List.filter (String.equal "@")
-    |> List.length
-    
+    |> List.length in
+    ((yi, xi), len)
 
 let inputs file =
   let contents = In_channel.with_open_bin file In_channel.input_all in
   String.split_on_char '\n' contents |> List.map (explode)
 
-let () = 
-  let map = inputs "inputs/day04" in
-   
+let rec replace_map replace_coords map = 
+  match replace_coords with 
+  | [] -> map
+  | (yi, xi) :: rest -> replace_map rest (replace_coord yi xi map)
+
+let get_replaceable_indices map =
   List.mapi (fun yi line -> List.mapi (fun xi _ -> get_neighboring_rolls yi xi map) line) map
     |> List.flatten
-    |> List.filter (fun e -> e < 4)
-    |> List.length
-    |> print_int
+    |> List.filter (fun ((a,b), e) -> e < 4)
+    |> List.map (fun ((a,b), _) -> (a,b))
+
+
+let rec replace_until_done (indices : (int * int) list) map replaced =
+  match indices with
+  | [] -> replaced
+  | _indices -> 
+      let new_map = replace_map _indices map in
+      let replaceable = get_replaceable_indices new_map in
+      replace_until_done replaceable new_map (replaced + List.length replaceable)
+
+
+let () = 
+  let map = inputs "inputs/day04" in
+  let indices = get_replaceable_indices map in
+  let map_after = replace_until_done indices map (List.length indices) in
+  print_int map_after
   
